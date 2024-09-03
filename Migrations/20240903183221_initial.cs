402155e210f3f3ae1631aed2727e6a37616f9da1
RodrigoBatis <rodrigofreirebatista@gmail.com>
2024-09-03 15:44:12 -0300

    New Project

---
+++ Migrations/20240903183221_initial.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Empresa.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departamentos_PX",
+                columns: table => new
+                {
+                    DepId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    DepNome = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamentos_PX", x => x.DepId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empregados_PX",
+                columns: table => new
+                {
+                    EmpId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Sobrenome = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Genero = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    DepartamentoDepId = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    FotoUrl = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empregados_PX", x => x.EmpId);
+                    table.ForeignKey(
+                        name: "FK_Empregados_PX_Departamentos_PX_DepartamentoDepId",
+                        column: x => x.DepartamentoDepId,
+                        principalTable: "Departamentos_PX",
+                        principalColumn: "DepId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empregados_PX_DepartamentoDepId",
+                table: "Empregados_PX",
+                column: "DepartamentoDepId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Empregados_PX");
+
+            migrationBuilder.DropTable(
+                name: "Departamentos_PX");
+        }
+    }
+}
